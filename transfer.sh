949b02533ea980e431b588a8b721b3580e66df6d
Edward Medvedev <edward.medvedev@gmail.com>
2016-07-31 10:32:07 +0300

    Almost working. :)

--- transfer.sh
+++ transfer.sh
@@ -3,38 +3,53 @@
 CONTRIB_REPO=https://github.com/StackStorm/st2contrib
 EXCHANGE_ORG=StackStorm-Exchange
 
-echo "Starting pack transfer from st2contrib to StackStorm Exchange."
-echo "=============================================================="
+echo "Starting pack transfer from st2contrib to StackStorm Exchange"
+echo "============================================================="
 echo
 
 read -p "GitHub username: " USERNAME
 read -sp "Password/token: " PASSWORD
 
+echo
+echo
+
+rm -rf /tmp/st2contrib
+rm -rf /tmp/stackstorm-exchange
+
 mkdir /tmp/stackstorm-exchange
 
+git clone $CONTRIB_REPO /tmp/st2contrib
+cd /tmp/st2contrib/packs
+
+echo
+
 for PACK in `ls`; do
-  echo -n "Moving $pack... "
-  git ls-remote https://a:a@github.com/$EXCHANGE_ORG/$PACK > /dev/null 2>&1
-  if [ "$?" -ne 0 ]; then
+  echo -n "Moving $PACK... "
+  git ls-remote https://$USERNAME:$PASSWORD@github.com/$EXCHANGE_ORG/$PACK > /dev/null 2>&1
+  if [ "$?" == 0 ]; then
   	echo "already there."
+  	echo
   	continue
   fi
 
+  echo
   mkdir /tmp/stackstorm-exchange/$PACK
   cd /tmp/stackstorm-exchange/$PACK
-  git clone $CONTRIB_REPO .
-  git remote set-url origin https://github.com/$EXCHANGE_ORG/$PACK.git
+  cp -R /tmp/st2contrib/. .
 
   git filter-branch --prune-empty --subdirectory-filter packs/$PACK master
+  git remote set-url origin https://$USERNAME:$PASSWORD@github.com/$EXCHANGE_ORG/$PACK.git
 
   chmod -R 775 .
   git add -A
-  git commit -am 'Transfer from st2contrib'
+  git commit -am 'Transfer from st2contrib' > /dev/null
   git push -u origin master
 
-  echo "done."
+  echo "The $PACK pack has been transferred."
+  echo
 done
 echo
 
+rm -rf /tmp/st2contrib
 rm -rf /tmp/stackstorm-exchange
 echo "All done."
