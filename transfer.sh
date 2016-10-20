ff96362d26c6c100f31d0288e0b952c50213aa49
Edward Medvedev <edward.medvedev@gmail.com>
2016-10-20 19:55:48 +0300

    Run the bootstrapping script

--- transfer.sh
+++ transfer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 CONTRIB_REPO=https://github.com/StackStorm/st2contrib
 EXCHANGE_ORG=StackStorm-Exchange
 
@@ -9,6 +11,7 @@ echo
 
 read -p "GitHub username: " USERNAME
 read -sp "Password/token: " PASSWORD
+read -sp "CircleCI token: " CIRCLECI_TOKEN
 
 echo
 echo
@@ -17,6 +20,8 @@ rm -rf /tmp/st2contrib
 rm -rf /tmp/stackstorm-exchange
 
 mkdir /tmp/stackstorm-exchange
+curl -sS --fail "https://raw.githubusercontent.com/StackStorm-Exchange/ci/master/utils/exchange-bootstrap.sh" > /tmp/stackstorm-exchange/bootstrap.sh
+chmod +x /tmp/stackstorm-exchange/bootstrap.sh
 
 git clone $CONTRIB_REPO /tmp/st2contrib
 cd /tmp/st2contrib/packs
@@ -25,14 +30,18 @@ echo
 
 for PACK in `ls`; do
   echo -n "Moving $PACK... "
-  git ls-remote https://$USERNAME:$PASSWORD@github.com/$EXCHANGE_ORG/$PACK > /dev/null 2>&1
-  if [ "$?" == 0 ]; then
+  if git ls-remote https://$USERNAME:$PASSWORD@github.com/$EXCHANGE_ORG/$PACK > /dev/null 2>&1
+  then
   	echo "already there."
   	echo
   	continue
   fi
 
   echo
+
+  /tmp/stackstorm-exchange/bootstrap.sh $PACK
+  echo "Bootstrapped an empty repo for $PACK."
+
   mkdir /tmp/stackstorm-exchange/$PACK
   cd /tmp/stackstorm-exchange/$PACK
   cp -R /tmp/st2contrib/. .
@@ -42,7 +51,8 @@ for PACK in `ls`; do
 
   chmod -R 775 .
   git add -A
-  git commit -am 'Transfer from st2contrib' > /dev/null
+  git commit -am 'Transfer from st2contrib.' > /dev/null
+  git merge origin/master
   git push -u origin master
 
   echo "The $PACK pack has been transferred."
