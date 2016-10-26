d38479cbb39d7d97110242a3da71a77195045d7d
Edward Medvedev <edward.medvedev@gmail.com>
2016-10-26 16:26:08 +0100

    Commit

--- transfer.sh
+++ transfer.sh
@@ -10,12 +10,15 @@ echo "Starting pack transfer from st2contrib to StackStorm Exchange"
 echo "============================================================="
 echo
 
-read -p "GitHub username: " USERNAME
-read -sp "Password/token: " PASSWORD
-read -sp "CircleCI token: " CIRCLECI_TOKEN
+#read -p "GitHub username: " USERNAME
+#read -sp "Password/token: " PASSWORD
+#read -sp "CircleCI token: " CIRCLECI_TOKEN
+#echo
+#echo
 
-echo
-echo
+USERNAME=$USERNAME
+PASSWORD=$PASSWORD
+CIRCLECI_TOKEN=$CIRCLECI_TOKEN
 
 rm -rf /tmp/st2contrib
 rm -rf /tmp/stackstorm-exchange
@@ -40,21 +43,25 @@ for PACK in `ls`; do
 
   echo
 
-  /tmp/stackstorm-exchange/bootstrap.sh $PACK
+  USERNAME=$USERNAME PASSWORD=$PASSWORD CIRCLECI_TOKEN=$CIRCLECI_TOKEN /tmp/stackstorm-exchange/bootstrap.sh $PACK >/dev/null
   echo "Bootstrapped an empty repo for $PACK."
 
   mkdir /tmp/stackstorm-exchange/$PACK
   cd /tmp/stackstorm-exchange/$PACK
   cp -R /tmp/st2contrib/. .
 
-  git filter-branch --prune-empty --subdirectory-filter packs/$PACK master
+  git filter-branch --prune-empty --subdirectory-filter packs/$PACK -f -- master
   git remote set-url origin https://$USERNAME:$PASSWORD@github.com/$EXCHANGE_ORG/$EXCHANGE_PREFIX-$PACK.git
+  git fetch --all -q
 
   chmod -R 775 .
-  git add -A
-  git commit -am 'Transfer from st2contrib.' > /dev/null
-  git merge origin/master
-  git push -u origin master
+  git commit -q -am 'Transfer from st2contrib.' > /dev/null
+  git merge -q origin/master -Xtheirs --allow-unrelated-histories --no-edit
+
+  git reset --soft HEAD~3 &&
+  git commit -q -m 'Transfer from st2contrib.'
+
+  git push -u origin master --force > /dev/null
 
   echo "The $PACK pack has been transferred."
   echo
